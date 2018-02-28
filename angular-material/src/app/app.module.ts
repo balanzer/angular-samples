156146b59d984c2fa8a09ea8a2f7540cdd7f2fda
Muralitharan Varatharajan <muralitharan.varatharajan@ihg.com>
2018-02-28 12:05:22 -0500

    Added Header, Footer and Stepper

--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+
 
 //hammerjs
 import  'hammerjs'
@@ -14,6 +16,18 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 //Input
 import {MatInputModule} from '@angular/material/input';
 
+//Menu 
+import {MatMenuModule} from '@angular/material/menu';
+
+//toolbar
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+//icon
+import {MatIconModule} from '@angular/material/icon';
+
+// stepper
+import {MatStepperModule} from '@angular/material/stepper';
+
 import { AppComponent } from './app.component';
 
 
@@ -23,8 +37,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule, 
+     FormsModule, ReactiveFormsModule,
     MatButtonModule, MatCheckboxModule,BrowserAnimationsModule,
-    MatSidenavModule, MatInputModule
+    MatSidenavModule, MatInputModule, MatMenuModule, MatToolbarModule,
+    MatIconModule, MatStepperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
